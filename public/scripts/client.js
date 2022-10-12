eda278a28bce2b3712b12dd785bf824eb10d1a0b
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-10-12 21:14:00 +0000

    Created a AJAX POST and GET method for the tweet submit form, and added form validation where it should not be empty or exceed the limit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,44 +5,17 @@
  */
 
 $(document).ready(() => {
-// Temporary tweetData
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+  const renderTweets = tweets => {
+    for (const tweet of tweets) {
+      $('.container').append(createTweetElement(tweet));
+    }
+  };
 
-const renderTweets = tweets => {
-  for (const tweet of tweets) {
-    $('.container').append(createTweetElement(tweet));
-  }
-};
+  const createTweetElement = tweet => {
+    const { user, content, created_at } = tweet;
+    // const dateCreated = Math.floor((Date.now() - created_at) / (1000 * 36000 * 24)); //change later
 
-const createTweetElement = tweet => {
-  const { user, content, created_at } = tweet;
-  const dateCreated = Math.floor((Date.now() - created_at) / (1000 * 36000 * 24)); //change later
-
-  const tweetContainer = `
+    const tweetContainer = `
     <article class="tweet">
       <header>
         <div>
@@ -57,7 +30,7 @@ const createTweetElement = tweet => {
         <p class="tweet">${content.text}</p>
       </div>
       <footer>
-        <p>${dateCreated} days ago</p>
+        <p>${timeago.format(created_at)}</p>
         <div class="icons">
           <i class="fa-sharp fa-solid fa-flag"></i>
           <i class="fa-sharp fa-solid fa-retweet"></i>
@@ -66,7 +39,31 @@ const createTweetElement = tweet => {
       </footer>
     </article>
   `;
-  return tweetContainer;
-};
-  renderTweets(data);
+    return tweetContainer;
+  };
+
+  const loadTweets = () => {
+    $.ajax({ url: '/tweets', method: 'GET' })
+      .then((res) => renderTweets(res));
+  };
+  loadTweets();
+
+  $('.tweet-form').submit(function(event) {
+    event.preventDefault();
+    const form = $(this);
+    const text = form.find('#tweet-text');
+    if (text.val().length > 140) {
+      alert('Your tweet exceeds the character limit of 140!');
+    } else if (text.val() === '' || text.val() === null) {
+      alert('Your tweet is empty!');
+    } else {
+      const serialized = form.serialize();
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: serialized
+      });
+      text.val('');
+    }
+  });
 });
